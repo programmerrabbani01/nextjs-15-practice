5ef2e40b90779dc06bacd7b9fc15dd55b3a9d997
programmerrabbani01 <programmerrabbani@gmail.com>
2024-11-11 18:59:17 +0600

    Class 1 Done

--- nextjs15-practice/actions/deVesAction.ts
+++ nextjs15-practice/actions/deVesAction.ts
@@ -3,6 +3,14 @@ import { cloudUpload } from "@/utils/cloudUpload.ts";
 import DeVs from "@/models/Developers.ts";
 import { mongoDBConnection } from "@/config/mongoDBConnect.ts";
 import { revalidatePath } from "next/cache";
+import cloudinary from "cloudinary";
+
+// Configure Cloudinary
+cloudinary.v2.config({
+  cloud_name: "dairwhedy",
+  api_key: "133861414556387",
+  api_secret: "VXfHSyH06s7z1XjH7dBxhMEid2Q",
+});
 
 // create a new developer
 export const createDevelopersData = async (formData: FormData) => {
@@ -20,6 +28,7 @@ export const createDevelopersData = async (formData: FormData) => {
     file: formData.get("photo") as File,
     preset: "mernStack-2",
     cloudName: "dairwhedy",
+    folder: "developers", // Developer-specific folder
   });
   // console.log(formData.get("photo"));
 
@@ -29,8 +38,8 @@ export const createDevelopersData = async (formData: FormData) => {
     cell,
     location,
     photo: {
-      url: fileData.secure_url,
-      public_id: fileData.public_id,
+      url: fileData[0].secure_url, // Retrieve from array of uploads
+      public_id: fileData[0].public_id,
     },
   });
 
@@ -52,22 +61,92 @@ export const getAllDevelopersData = async () => {
 
 // delete a developer
 
-export const deleteADeveloperData = async (id) => {
+export const deleteADeveloperData = async (id: string) => {
   await mongoDBConnection();
 
-  // Delete the developer
-  const deleteDeveloper = await DeVs.findByIdAndDelete(id);
+  const developer = await DeVs.findById(id);
+  if (!developer) return { success: false };
 
-  // Revalidate the path to refresh data on the client side
-  revalidatePath("/developers");
-
-  // Return a plain object if needed, or simply return a success status
-  if (deleteDeveloper) {
-    return {
-      success: true,
-      id: deleteDeveloper._id.toString(), // Convert `_id` to string
-    };
+  // Delete the photo from Cloudinary
+  if (developer?.photo?.public_id) {
+    await cloudinary.v2.uploader.destroy(developer.photo.public_id);
   }
 
-  return { success: false };
+  // Remove the developer from the database
+  await DeVs.findByIdAndDelete(id);
+  revalidatePath("/developers");
+
+  return { success: true };
 };
+
+// create a product
+
+// export const createProductData = async (formData: FormData) => {
+//   await mongoDBConnection();
+
+//   const name = formData.get("name");
+//   const price = formData.get("price");
+
+//   const productId = new ObjectId();
+//   // Generate unique ID for folder structure
+//   const folder = `products/${productId}`; // Unique folder for each product
+
+//   const files = formData.getAll("photos") as File[];
+//   const fileData = await cloudUpload({
+//     files,
+//     preset: "mernStack-2",
+//     cloudName: "dairwhedy",
+//     folder,
+//   });
+
+//   const photos = fileData.map((file) => ({
+//     url: file.secure_url,
+//     public_id: file.public_id,
+//   }));
+
+//   await Products.create({ name, price, photos });
+
+//   revalidatePath("/products");
+// };
+
+// update a product
+// export const updateProductData = async (id, formData: FormData) => {
+//   await mongoDBConnection();
+
+//   const product = await Products.findById(id);
+//   if (!product) return { success: false };
+
+//   const files = formData.getAll("photos") as File[];
+//   const newPhotos: any[] = [];
+//   const folder = `products/${id}`;
+
+//   if (files.length) {
+//     // Delete old photos from Cloudinary
+//     for (const photo of product.photos) {
+//       await cloudinary.v2.uploader.destroy(photo.public_id);
+//     }
+
+//     // Upload new photos
+//     const fileData = await cloudUpload({
+//       files,
+//       preset: "mernStack-2",
+//       cloudName: "dairwhedy",
+//       folder,
+//     });
+
+//     newPhotos.push(
+//       ...fileData.map((file) => ({
+//         url: file.secure_url,
+//         public_id: file.public_id,
+//       }))
+//     );
+//   }
+
+//   await Products.findByIdAndUpdate(id, {
+//     photos: files.length ? newPhotos : product.photos,
+//   });
+
+//   revalidatePath(`/products/${id}`);
+
+//   return { success: true };
+// };
--- nextjs15-practice/utils/cloudUpload.ts
+++ nextjs15-practice/utils/cloudUpload.ts
@@ -1,22 +1,41 @@
 import axios from "axios";
 
 interface CloudUploadParams {
-  file: File; // `file` should be of type `File` (usually from an input element)
-  preset: string; // `preset` should be a `string`
-  cloudName: string; // `cloudName` should be a `string`
+  file: File; // For single file uploads (e.g., developer photo)
+  files?: File[]; // For multiple files, e.g., product photos
+  preset: string;
+  cloudName: string;
+  folder: string; // Dynamic folder based on entity type
 }
 
 export const cloudUpload = async ({
   file,
+  files,
   preset,
   cloudName,
+  folder,
 }: CloudUploadParams) => {
-  const form_data = new FormData();
-  form_data.append("file", file);
-  form_data.append("upload_preset", preset);
-  const res = await axios.post(
-    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-    form_data
-  );
-  return res.data;
+  // const form_data = new FormData();
+  // form_data.append("file", file);
+  // form_data.append("upload_preset", preset);
+  // const res = await axios.post(
+  //   `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+  //   form_data
+  // );
+  // return res.data;
+
+  const uploads = (files || [file]).map(async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", preset);
+    formData.append("folder", folder); // Specify folder
+
+    const res = await axios.post(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      formData
+    );
+    return res.data; // Returns secure_url and public_id
+  });
+
+  return Promise.all(uploads); // For multiple or single files
 };
